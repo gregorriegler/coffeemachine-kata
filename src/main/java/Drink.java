de1c98f97a50bc213df940531d7b4bfff120764c
Gregor Riegler <gregor.riegler@ebcont.com>
2019-12-28 21:27:55 +0100

    formatting

--- src/main/java/Drink.java
+++ src/main/java/Drink.java
@@ -1,56 +1,56 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Drink implements Comparable<Drink>{
+public class Drink implements Comparable<Drink> {
     private Map<String, Integer> recipe = new HashMap<String, Integer>();//map ingredients to units per
     private String name;
     private double totalCost = 0;
     private boolean makeable = false;
 
-    public Drink(String name, String[] recipe){
+    public Drink(String name, String[] recipe) {
         this.name = name;
         setRecipe(recipe);
     }
 
-    public int compareTo(Drink drink){
+    public int compareTo(Drink drink) {
         return name.compareTo(drink.getName());
     }
 
-    public void setRecipe(String[] recipe){
-        for(String s : recipe){
-            if(this.recipe.containsKey(s)){
-                this.recipe.put(s, this.recipe.get(s)+1);//increment if multiple units
-            }else{
+    public void setRecipe(String[] recipe) {
+        for (String s : recipe) {
+            if (this.recipe.containsKey(s)) {
+                this.recipe.put(s, this.recipe.get(s) + 1);//increment if multiple units
+            } else {
                 this.recipe.put(s, 1);//insert first occurrence of ingredient
             }
         }
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setCost(double totalCost){
+    public void setCost(double totalCost) {
         this.totalCost = totalCost;
     }
 
-    public void setMakeable(boolean makeable){
+    public void setMakeable(boolean makeable) {
         this.makeable = makeable;
     }
 
-    public Map<String, Integer> getRecipe(){
+    public Map<String, Integer> getRecipe() {
         return recipe;
     }
 
-    public double getCost(){
+    public double getCost() {
         return totalCost;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public boolean getMakeable(){
+    public boolean getMakeable() {
         return makeable;
     }
 
--- src/main/java/DrinkMachine.java
+++ src/main/java/DrinkMachine.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class DrinkMachine{
+public class DrinkMachine {
 
     private static List<Drink> drinkList = new ArrayList<Drink>();
     private static List<Ingredient> ingredientList = new ArrayList<Ingredient>();
@@ -20,11 +20,11 @@ public class DrinkMachine{
         startIO();
     }
 
-    public static void startIO(){
+    public static void startIO() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = "";
         //running loop
-        while(true){            
+        while (true) {
             try {
                 /*I considered using a switch here for more readable code. Elected to have a dynamic drink menu so drinks can be added
                  * to addAllDrinks() and this loop would not have to be change. The decision to have slightly less readable code
@@ -32,44 +32,46 @@ public class DrinkMachine{
                  */
                 input = reader.readLine().toLowerCase();
 
-                if(input.equals("")){
+                if (input.equals("")) {
                     continue;
-                }else if (input.equals("q")){ 
-                    System.exit(0); 
-                }else if(input.equals("r")){ 
+                } else if (input.equals("q")) {
+                    System.exit(0);
+                } else if (input.equals("r")) {
                     restockIngredients();
                     updateMakeable();
-                }else if(Integer.parseInt(input) > 0 && Integer.parseInt(input) <= drinkList.size()){ //dynamic drink menu selection
-                    makeDrink(drinkList.get(Integer.parseInt(input)-1));
-                }else{
+                } else if (Integer.parseInt(input) > 0 && Integer.parseInt(input) <= drinkList.size()) { //dynamic drink menu selection
+                    makeDrink(drinkList.get(Integer.parseInt(input) - 1));
+                } else {
                     throw new IOException();//legal, but invalid input
                 }
             } catch (Exception e) {
-                System.out.println("Invalid selection: " + input + "\n");//illegal input
+                System.out.print("Invalid selection: " + input + ". Try again: ");//illegal input
             }
         }//running loop     
     }
 
-    public static void display(){
+    public static void display() {
         //The sample IO in the assignment appears to add a blank line after output, so I included that.
-        System.out.println("Inventory:\n");
-        for (Ingredient i : ingredientList){
-            System.out.println(i.getName() + "," + i.getStock() + "\n");
+        System.out.println("Inventory:");
+        for (Ingredient i : ingredientList) {
+            System.out.println(i.getName() + "," + i.getStock());
         }
 
-        System.out.println("Menu:\n");
+        System.out.println("\nMenu:");
         int count = 1;
-        for (Drink d : drinkList){
-            System.out.printf("%d,%s,$%.2f," + d.getMakeable() + "\n\n", count, d.getName(), d.getCost());
+        for (Drink d : drinkList) {
+            System.out.printf("%d,%s,$%.2f," + d.getMakeable() + "\n", count, d.getName(), d.getCost());
             count++;
         }
+
+        System.out.print("\nYour selection: ");
     }
 
-    public static void updateMakeable(){
-        for (Drink d : drinkList){
+    public static void updateMakeable() {
+        for (Drink d : drinkList) {
             Map<String, Integer> currRecipe = d.getRecipe();
-            for (Ingredient i : ingredientList){
-                if (currRecipe.containsKey(i.getName()) && i.getStock() < currRecipe.get(i.getName())){
+            for (Ingredient i : ingredientList) {
+                if (currRecipe.containsKey(i.getName()) && i.getStock() < currRecipe.get(i.getName())) {
                     d.setMakeable(false);
                     break;//check next drink
                 }
@@ -78,36 +80,36 @@ public class DrinkMachine{
         }//Drink loop
     }
 
-    public static void updateCosts(){
-        for (Drink d : drinkList){
+    public static void updateCosts() {
+        for (Drink d : drinkList) {
             double currCost = 0;
             Map<String, Integer> currRecipe = d.getRecipe();
-            for (Ingredient i : ingredientList){
-                if (currRecipe.containsKey(i.getName())){
-                    currCost += i.getCost()*currRecipe.get(i.getName());
+            for (Ingredient i : ingredientList) {
+                if (currRecipe.containsKey(i.getName())) {
+                    currCost += i.getCost() * currRecipe.get(i.getName());
                 }
             }//inner
             d.setCost(currCost);
         }//outer
     }
 
-    public static void makeDrink(Drink drink){
-        if(drink.getMakeable()){
+    public static void makeDrink(Drink drink) {
+        if (drink.getMakeable()) {
             System.out.println("Dispensing: " + drink.getName() + "\n");
-            for (Ingredient i : ingredientList){
-                if(drink.getRecipe().containsKey(i.getName())){
-                    i.setStock(i.getStock()-drink.getRecipe().get(i.getName()));
+            for (Ingredient i : ingredientList) {
+                if (drink.getRecipe().containsKey(i.getName())) {
+                    i.setStock(i.getStock() - drink.getRecipe().get(i.getName()));
                 }
             }
-        }else{
+        } else {
             System.out.println("Out of stock: " + drink.getName() + "\n");
         }
         updateMakeable();
         display();
     }
 
-    public static void restockIngredients(){
-        for(Ingredient i : ingredientList){
+    public static void restockIngredients() {
+        for (Ingredient i : ingredientList) {
             i.setStock(10);
         }
         updateMakeable();
@@ -115,15 +117,16 @@ public class DrinkMachine{
     }
 
     //Add ingredients through addAllIngredients
-    public static void addIngredient(Ingredient ingredient){
+    public static void addIngredient(Ingredient ingredient) {
         ingredientList.add(ingredient);
     }
+
     //Add drinks through addAllDrinks
-    public static void addDrink(String name, String[] recipe){
+    public static void addDrink(String name, String[] recipe) {
         drinkList.add(new Drink(name, recipe));
     }
 
-    public static void addAllIngredients(){
+    public static void addAllIngredients() {
         addIngredient(new Ingredient("Coffee", 0.75));
         addIngredient(new Ingredient("Decaf Coffee", 0.75));
         addIngredient(new Ingredient("Sugar", 0.25));
@@ -132,12 +135,12 @@ public class DrinkMachine{
         addIngredient(new Ingredient("Foamed Milk", 0.35));
         addIngredient(new Ingredient("Espresso", 1.10));
         addIngredient(new Ingredient("Cocoa", 0.90));
-        addIngredient(new Ingredient("Whipped Cream", 1.00));   
+        addIngredient(new Ingredient("Whipped Cream", 1.00));
 
         Collections.sort(ingredientList);
     }
 
-    public static void addAllDrinks(){  
+    public static void addAllDrinks() {
         addDrink("Coffee", new String[]{"Coffee", "Coffee", "Coffee", "Sugar", "Cream"});
         addDrink("Decaf Coffee", new String[]{"Decaf Coffee", "Decaf Coffee", "Decaf Coffee", "Sugar", "Cream"});
         addDrink("Caffe Latte", new String[]{"Espresso", "Espresso", "Steamed Milk"});
